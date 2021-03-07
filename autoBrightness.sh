075a1b0eef422648a86eebbbc9daed1b7a1185a6
Federico Maggiore <46093990+fedmag@users.noreply.github.com>
2021-03-07 09:58:00 +0100

    Update autoBrightness.sh

--- autoBrightness.sh
+++ autoBrightness.sh
@@ -1,24 +1,29 @@
-#!/usr/bin/bash 
+#!/bin/bash
 
-Time=$(date +%H)
-while true
-do
-	if [ $Time -lt 7 ] || [ $Time -gt 20 ] #lt less than gt greater than -> night hours
-	then
-		xrandr --output HDMI-1 --brightness 0.3
-		xrandr --output HDMI-0 --brightness 0.3
-		echo "Minimum brightness"
-	elif  [ $Time -gt 7 ] && [ $Time -lt 11 ] || [ $Time -gt 18 ] && [ $Time -lt 20 ] 
-	then 
-		xrandr --output HDMI-1 --brightness 0.6
-		xrandr --output HDMI-0 --brightness 0.6
-		echo "Medium brightness"
-	else
-		xrandr --output HDMI-1 --brightness 1
-		xrandr --output HDMI-0 --brightness 1
-		echo "Maximum brightness"
-	fi
-	sleep 3600
+# While the user is not logged in == until the $DISPLAY variable is unset or empty
+unset DISPLAY
+while [ -z "$DISPLAY" ] || [ "$DISPLAY" == "" ]; do
+        DISPLAY=$(w "$(id -un)" | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}' 2>/dev/null)
+        if [ "$DISPLAY" == "" ]; then sleep 30; else export DISPLAY="$DISPLAY"; fi
 done
 
+brightness(){
+        # Get the list of the active monitors automatically
+        # To set this list manually use: OUT=( VGA-1 HDMI-1 HDMI-2 HDMI-3 )
+        OUT=$(xrandr --listactivemonitors | awk 'NR!=1{print " "$NF" "}')
+        # Adjust the brightness level for each monitor
+        for current in "${OUT[@]}"; do xrandr --output "${current// /}" --brightness "$1"; done
+}
+
+if [ -z "${1+x}" ]; then  # If the scrip is called from Cron or CLI without an argument: 'brightness'
+        H=$(date +%-H)
+        if   ((  0 <= "$H" && "$H" <  7 )); then brightness ".5"
+        elif ((  7 <= "$H" && "$H" < 10 )); then brightness ".6"
+        elif (( 10 <= "$H" && "$H" < 19 )); then brightness ".7"
+        elif (( 19 <= "$H" && "$H" < 22 )); then brightness ".6"
+        elif (( 22 <= "$H" && "$H" < 24 )); then brightness ".5"
+        else echo "Error"
+        fi
+else brightness "$1"    # If the scipt is called with an additional argument: 'brightness "<value>"'
+fi
 
